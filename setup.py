d7d72e8ddaff7888590ebe3e3cdb01f89462d4a8
Filipe Varela <fcvarela@gmail.com>
2013-01-22 14:09:45 +0000

    Packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='pyremotezip',
+	version=version,
+	description="Extract files from remote ZIP archives",
+	long_description=""" """,
+	classifiers=[],
+	keywords="",
+	author="Filipe Varela",
+	author_email="fcvarela@gmail.com",
+	url="https://github.com/fcvarela/pyremotezip/",
+	license="BSD",
+	package_dir={'': '.'},
+	packages=find_packages(where='.'),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=['setuptools',],
+	entry_points="""
+	# None whatsoever
+	""",
+)
